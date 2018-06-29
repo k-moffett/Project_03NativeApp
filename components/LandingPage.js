0c8b15d598565fdab181a4112a5ee71a0c8b0d1a
Kurt <moffkr91@gmail.com>
2018-06-29 14:26:49 -0700

    Landing, Register, Login Working
    These three components are now working. We need to start working on the
    Homepage now, as well as creating user auth.

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
+import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native'
 
 export default class LandingPage extends React.Component {
     constructor(props) {
@@ -8,30 +8,72 @@ export default class LandingPage extends React.Component {
 
     render() {
         return (
-            <View>
+            <View style={styles.container}>
 
-		<View style={styles.filler} ></View>
+		<Image 
+		    source={require('../images/SurrounDM.png')}
+		    style={styles.logo}
+		/>
 
-                <TouchableHighlight
+		<TouchableHighlight
                     onPress={(e) => {this.props.goTo('Register')}}
                     style={styles.button}>
-                    <Text>Create Account</Text>
+                    <Text style={styles.btntext}>Create Account</Text>
                 </TouchableHighlight>
 
                 <TouchableHighlight
                     onPress={(e) => {this.props.goTo('Login')}}
                     style={styles.button}>
-                    <Text>Login</Text>
+                    <Text style={styles.btntext} >Login</Text>
                 </TouchableHighlight>
 
+		<Text style={styles.textinput} >Forgot Password?</Text>
             </View>
         );
     }
 }
 
-const styles = {
-    filler: {
-	    marginTop: '5%',
-    }
-}
+const styles = StyleSheet.create({
+   
+    container: {
+        backgroundColor: '#29377E',
+        alignItems: 'center',
+        justifyContent: 'center',
+	height: '100%',
+    },
+    logo: {
+        width: 250,
+        height: 40,
+        marginBottom: 40
 
+    },
+    header: {
+        fontSize: 24,
+        color: '#fff',
+        paddingBottom: 10,
+        marginBottom: 40,
+        borderBottomColor: '#199187',
+        borderBottomWidth: 1
+    },
+    textinput: {
+        alignSelf: 'stretch',
+        height: 40,
+        marginBottom: 30,
+        color: '#fff',
+        borderBottomColor: '#f8f8f8',
+        borderBottomWidth: 1,
+    },
+    button: {
+        alignSelf: 'stretch',
+        alignSelf: 'center',
+        padding: 20,
+        backgroundColor: '#59cbbd',
+        marginTop: 30,
+        borderRadius: 5
+    },
+    btntext: {
+        color: '#fff',
+        fontWeight: 'bold'
+    }
+
+});
--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableHighlight, TextInput } from 'react-native'
+import { StyleSheet, Text, View, TouchableHighlight, TextInput, Image } from 'react-native'
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -33,26 +33,92 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <View>
+            <View style={styles.container}>
+
+	        <Image 
+		    source={require('../images/SurrounDM.png')}
+		    style={styles.logo}
+		/>
+
+		<Text style={styles.header}>Login</Text>
 
 		<TextInput
                     placeholder="email"
-                    underlineColorAndroid={'transparent'}
+                    style={styles.textinput}
+		    underlineColorAndroid={'transparent'}
                     onChangeText={(email) => this.setState({ email })}
                     value={this.state.email}
                 />
                 <TextInput
                     placeholder="password"
-                    underlineColorAndroid={'transparent'}
+                    style={styles.textinput}
+		    underlineColorAndroid={'transparent'}
                     onChangeText={(password) => this.setState({ password })}
                     value={this.state.password}
                 /> 
 
-                <TouchableHighlight onPress={(e) => {this.submit()}}>
-                    <Text>Login</Text>
+                <TouchableHighlight 
+		    onPress={(e) => {this.submit()}}
+		    style={styles.button}>
+                    <Text syle={styles.btntext}>Login</Text>
                 </TouchableHighlight>
-
-            </View>
+               
+		<TouchableHighlight 
+		    onPress={(e) => {this.props.goTo('Landing')}}
+		    style={styles.button}>
+                    <Text syle={styles.btntext}>Back</Text> 
+		</TouchableHighlight>
+           
+	    </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+	height: '100%',
+        backgroundColor: '#29377E',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 250,
+        height: 40,
+        marginBottom: 40
+
+    },
+    regform: {
+        alignSelf: 'stretch',
+    },
+    header: {
+        fontSize: 24,
+        color: '#fff',
+        paddingBottom: 10,
+        marginBottom: 40,
+        borderBottomColor: '#199187',
+        borderBottomWidth: 1
+    },
+    textinput: {
+        alignSelf: 'stretch',
+	alignSelf: 'center',
+        height: 40,
+	width: '60%',
+        marginBottom: 30,
+        color: '#fff',
+        borderBottomColor: '#f8f8f8',
+        borderBottomWidth: 1,
+    },
+    button: {
+        alignSelf: 'stretch',
+        alignSelf: 'center',
+        padding: 20,
+        backgroundColor: '#59cbbd',
+        marginTop: 30,
+        borderRadius: 5
+    },
+    btntext: {
+        color: '#fff',
+        fontWeight: 'bold'
+    }
+});
