22ca918c3498c8581cc80048600789260ecd5cd3
mohammadse2 <mpowerelectronic@gmail.com>
2018-07-14 13:48:04 -0700

    ms

--- components/homePageComponents/MainView.js
+++ components/homePageComponents/MainView.js
@@ -5,32 +5,24 @@ export default class MainView extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = { text: 'Enter Chat Name Here' }
+        this.state = { text: '' }
     }
 
-    componentWillMount() {
-        this.getUserInfo()
+    checkSessid() {
+        console.log('SESSID: ', this.props.sessid)
     }
 
-    getUserInfo() {
-        fetch('http://app.surroundm.com/login', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                sessid: this.props.sessid,
-            })
+    logout() {
+        this.props.goTo('LandingPage')
+    }
 
-        })
-        .then((response) => response.json())
-        .then((responseJson) => {
-            console.log(responseJson)
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    enterChat() {
+        this.props.goTo('ChatRoom')
+    }
+
+    addChat() {
+        console.log('create chat in firebase named: ', this.state.text)
+        console.log('query firebase to update all chats in the scoll view')
     }
 
     logout() {
@@ -41,7 +33,7 @@ export default class MainView extends React.Component {
         console.log('create chat in firebase named: ', this.state.text)
         console.log('query firebase to update all chats in the scoll view')
     }
-    
+
     checkSessid() {
         console.log('SESSID: ', this.props.sessid)
     }
@@ -52,106 +44,201 @@ export default class MainView extends React.Component {
 
     render() {
 
-        return(
-            <View>
+        return (
+            <View style={styles.container}>
+                <View style={styles.container1}>
+                    <Image
+                        style={styles.flexitemLogo}
+                        source={require('./../../images/SurrounDM.png')}
+                    />
 
-                <TouchableHighlight
-                    style={styles.button}
-                    onPress={(e) => { this.logout() }}
-                >
-                    <Text style={styles.btntext}>Log Out</Text>
-                </TouchableHighlight>
+                    <TouchableHighlight
+                        style={styles.flexitem1}
+                        onPress={(e) => { this.logout() }}
+                    >
+                        <Text style={styles.btntext}>Log Out</Text>
+                    </TouchableHighlight>
 
-                <TextInput 
-                    onChangeText={(text) => this.setState({text})}
-                    value={this.state.text}
-                />
+                    <TextInput
+                        style={styles.flexitem2}
+                        onChangeText={(text) => this.setState({ text })}
+                        value={this.state.text}
+                        placeholder='Enter Chat Name Here'
+                        placeholderTextColor='#636e72'
+                        underlineColorAndroid={'transparent'}
 
-                <TouchableHighlight
-                    onPress={(e) => {this.addChat()}}
-                >
-                <Text>Add Chat</Text>
-                </TouchableHighlight>
+                    />
 
-                <Text>Chatrooms around me within: 1 mile</Text>
+                    <TouchableHighlight
+                        style={styles.flexitem3}
+                        onPress={(e) => { this.addChat() }}
+                    >
+                        <Text style={styles.btntext}>Add Chat</Text>
+                    </TouchableHighlight>
 
-                <ScrollView>
-                    {/* {[...Array(100)].map((item, index) => {
+                    <Text style={styles.chatText}>Chatrooms around me within: 1 mile</Text>
+                </View>
+
+                <View style={styles.container2}>
+                    <ScrollView>
+                        {/* {[...Array(100)].map((item, index) => {
                         return(
                             <View>
                                 <Text>{index}</Text>
                             </View>
                         )
                     })} */}
+                        <TouchableHighlight
+                            style={styles.flexitem4}
+                            onPress={(e) => { console.log('button click motherfucker.') }}
+                        >
+                            <Text>This is what a chatroom button will look like</Text>
+                        </TouchableHighlight>
 
-                <TouchableHighlight
-                    onPress={(e) => {console.log('button click motherfucker.')}}
-                >
-                <Text>This is what a chatroom button will look like</Text>
-                </TouchableHighlight>
-
-                </ScrollView>
-
-                {/*Test*/}
-                <TouchableHighlight
-                    onPress={(e) => {this.checkSessid()}}
-                >
-                <Text>Sessid</Text>
-                </TouchableHighlight>
+                    </ScrollView>
+                </View>
 
                 {/*Test*/}
                 <TouchableHighlight
-                    onPress={(e) => {this.enterChat()}}
+                    style={styles.flexitem5}
+                    onPress={(e) => { this.checkSessid() }}
                 >
-                <Text>Enter Chat</Text>
+                    <Text>Sessid</Text>
                 </TouchableHighlight>
 
-	        </View>
+                {/*Test*/}
+                <TouchableHighlight
+                    style={styles.flexitem6}
+                    onPress={(e) => { this.enterChat() }}
+                >
+                    <Text>Enter Chat</Text>
+                </TouchableHighlight>
+
+            </View>
         )
     }
 }
 
-
 const styles = StyleSheet.create({
-
     container: {
         backgroundColor: '#29377E',
-        // alignItems: 'center',
-        // justifyContent: 'center',
         height: '100%',
     },
-    logo: {
-      
+
+
+    container1: {
+        backgroundColor: '#29377E',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        // justifyContent: 'space-between',
+        // height: '100%',
+    },
+    flexitemLogo: {
+        // alignItems: 'center',
+        // justifyContent: 'center',
         width: 200,
         height: 30,
+        marginTop: 50,
         marginBottom: 20,
+        // marginLeft: 90,
+        marginLeft: 'auto',
+        marginRight: 'auto',
     },
-    button: {
-        // alignSelf: 'stretch',
-        // alignSelf: 'center',
-        // width: 200,
-        // height: 5,
-        // padding: 10,
-        // backgroundColor: '#59cbbd',
-        // marginTop: 30,
-        // borderRadius: 5
+    flexitem1: {
+        // backgroundColor: '#426bd7',
+        alignItems: 'center',
+        marginRight: 11,
+        height: 30,
+        width: 70,
+        justifyContent: 'center',
+        borderRadius: 5,
+        marginBottom: 20,
+        marginTop: 50,
+    },
 
+
+    flexitem2: {
+        backgroundColor: '#dfe6e9',
         justifyContent: 'center',
         alignItems: 'center',
-        margin: 10,
-        marginRight: 25,
-        marginLeft: 25,
-        padding: 10,
-
-        backgroundColor: '#426bd7',
         borderRadius: 5,
-        width: 100,
+        margin: 2,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        height: 50,
+        width: 320,
+        padding: 15,
+        fontSize: 17
     },
+    flexitem3: {
+        backgroundColor: '#426bd7',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        height: 50,
+        width: 70,
+    },
+
+
+    container2: {
+        backgroundColor: '#29377E',
+        flexDirection: 'row',
+        // flexWrap: 'wrap',
+        // justifyContent: 'center',
+        // height: '100%',
+    },
+    flexitem4: {
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 20,
+        marginBottom: 20,
+        height: 50,
+        width: '100%',
+        borderWidth: 0.5,
+        borderColor: '#636e72',
+    },
+
+    flexitem5: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        margin: 2,
+
+    },
+    flexitem6: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        margin: 2
+    },
+
+
     btntext: {
-        color: '#fff',
-        fontWeight: 'bold'
+        color: '#fff'
+    },
+    chatText: {
+        color: 'white',
+        textAlign: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 20,
+        marginTop: 20,
+        fontSize: 18
     }
 })
+
+
+
+
+
+
+
+
+
+
 
 
 
